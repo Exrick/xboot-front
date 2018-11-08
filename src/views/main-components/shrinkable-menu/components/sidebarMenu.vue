9080792a06c8a8497fab1bede10e682a009dafc7
bugall <769088641@qq.com>
2018-11-08 17:51:06 +0800

    Update sidebarMenu.vue
    当二级菜单只有一个的时候一级菜单无法正常显示

--- src/views/main-components/shrinkable-menu/components/sidebarMenu.vue
+++ src/views/main-components/shrinkable-menu/components/sidebarMenu.vue
@@ -4,12 +4,12 @@
 <template>
     <Menu ref="sideMenu" accordion :active-name="$route.name" :open-names="singleOpenName" :theme="menuTheme" width="auto" @on-select="changeMenu">
         <template v-for="item in menuList">
-            <MenuItem v-if="item.children.length<=1" :name="item.children[0].name" :key="'menuitem' + item.name">
+            <MenuItem v-if="item.children.length < 1" :name="item.children[0].name" :key="'menuitem' + item.name">
                 <Icon :type="item.children[0].icon || item.icon" :size="iconSize" :key="'menuicon' + item.name"></Icon>
                 <span class="layout-text" :key="'title' + item.name">{{ itemTitle(item.children[0]) }}</span>
             </MenuItem>
 
-            <Submenu v-if="item.children.length > 1" :name="item.name" :key="item.name">
+            <Submenu v-if="item.children.length >= 1" :name="item.name" :key="item.name">
                 <template slot="title">
                     <Icon :type="item.icon" :size="iconSize"></Icon>
                     <span class="layout-text">{{ itemTitle(item) }}</span>
